250a7d530d0f6e37b54110446a0d234bb1f7978d
Pranav <pranavkumar192192@gmail.com>
2023-09-11 17:42:46 +0530

    homepage

---
+++ script.js
@@ -0,0 +1,5 @@
+let NavToggle = document.getElementById('SlideBar-toggle-btn')
+NavToggle.addEventListener('click' , function(){
+    document.getElementById('SlideBar').classList.toggle('active')
+})
+
